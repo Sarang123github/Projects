72ebeebc5acd3eacfeb16ffcbdb68071e3b9b778
unknown <bhongsarang007@gmail.com>
2022-09-14 21:12:54 +0530

    i change some code

--- Sarangptoject/src/com/project/Class.java
+++ Sarangptoject/src/com/project/Class.java
@@ -1,16 +1,17 @@
 package com.project;
 
 import java.util.Arrays;
-import java.util.Iterator;
+
 import java.util.List;
 
 public class Class {
 
 	public static void main(String[] args) {
 		List<String> asList = Arrays.asList("sarang","sagar","aai");
-		Iterator<String> iterator = asList.iterator();
-		while(iterator.hasNext()){
-			System.out.println(iterator.next());
+		
+		
+		for ( String i: asList ) {
+			System.out.println(i);
 
 	}
 
