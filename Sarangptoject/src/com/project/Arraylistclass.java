9e881787709237b18a04b08c7501d1c8f9edbd41
unknown <bhongsarang007@gmail.com>
2022-09-15 11:17:45 +0530

    new code

---
+++ Sarangptoject/src/com/project/Arraylistclass.java
@@ -0,0 +1,22 @@
+package com.project;
+
+import java.util.HashSet;
+
+public class Arraylistclass {
+
+	public static void main(String[] args) {
+HashSet <Integer> set =new HashSet <Integer>();
+		
+		set.add(1);
+		set.add(3);
+		set.add(50);
+		set.add(70);
+		set.add(90);
+		
+		for (Integer i:set) {
+			System.out.println(i);
+
+	}
+
+}
+}
